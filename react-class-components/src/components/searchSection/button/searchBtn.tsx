b5aca9a8f74cd1780e8ba752b91f5af01ce8b229
Svetlana <svetailina1987@gmail.com>
2024-07-04 15:56:18 +0400

    fix: correct classes name

--- react-class-components/src/components/searchSection/button/searchBtn.tsx
+++ react-class-components/src/components/searchSection/button/searchBtn.tsx
@@ -4,7 +4,7 @@ interface SearchButtonProps {
     onClick: () => void;
 }
 
-export default class SearcButton extends Component<SearchButtonProps> {
+export default class SearchButton extends Component<SearchButtonProps> {
     render() {
         return <button onClick={this.props.onClick}>Search</button>;
     }
