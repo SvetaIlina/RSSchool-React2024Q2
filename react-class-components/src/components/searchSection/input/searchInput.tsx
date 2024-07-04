4bd5be3cca986ecfc0983593ad75670adbfed2e4
Svetlana <svetailina1987@gmail.com>
2024-07-04 15:49:27 +0400

    feat: add input component

---
+++ react-class-components/src/components/searchSection/input/searchInput.tsx
@@ -0,0 +1,28 @@
+import React, { ChangeEvent, Component } from 'react';
+import './searchInput.css';
+
+interface SearchInputProps {
+    value: string;
+    onChange: (value: string) => void;
+}
+export default class SearchInput extends Component<SearchInputProps> {
+    constructor(props: SearchInputProps) {
+        super(props);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+        this.props.onChange(event.target.value);
+    }
+    render(): React.ReactNode {
+        return (
+            <input
+                className="search-input"
+                type="text"
+                value={this.props.value}
+                onChange={this.handleInputChange}
+                placeholder="Search..."
+            />
+        );
+    }
+}
