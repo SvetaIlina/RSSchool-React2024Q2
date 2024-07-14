28e9a020ecf36dcc6cae725120de972c344071f3
Svetlana <svetailina1987@gmail.com>
2024-07-14 16:05:57 +0300

    feat: add routing

--- react-class-components/src/components/app/App.tsx
+++ react-class-components/src/components/app/App.tsx
@@ -1,73 +1,23 @@
-import { useEffect, useState } from 'react';
-import { SwapiPerson } from '../../types/type';
-import ResultsSection from '../resultSection/resultSection';
-import SearchSection from '../searchSection/searchSection';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './app.css';
-import ErrorImitationBtn from '../errorBoundary/errorImitationButton/errorImitationButton';
-import fetchData from '../../utils/swapi';
-import useSavedQuery from '../../hooks/useSavedQuery';
+import MainPage from '../../pages/main/mainPage';
+import React from 'react';
+import NotFoundPage from '../../pages/notFound/notFoundPage';
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainPage />,
+    },
+    {
+        path: '*',
+        element: <NotFoundPage />,
+    },
+]);
 
 export default function App() {
-    const [savedQuery, setSavedQuery] = useSavedQuery('searchTerm');
-    const [searchTerm, setSearchTerm] = useState('');
-    const [searchResult, setSearchResult] = useState<SwapiPerson[]>([]);
-    const [errorCounter, setErrorCounter] = useState(0);
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        async function displayResult() {
-            let term: string = searchTerm;
-            if (savedQuery) {
-                setSearchTerm(savedQuery);
-                term = savedQuery;
-            }
-            await getResults(term);
-        }
-
-        displayResult();
-    }, []);
-
-    useEffect(() => {
-        if (errorCounter > 1) {
-            throw new Error('I crashed!');
-        }
-    }, [errorCounter]);
-
-    const getResults = async (term: string) => {
-        setIsLoading(true);
-
-        try {
-            const result = await fetchData(term);
-            setSearchResult(result);
-        } catch (error) {
-            console.error(error);
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
-    const handleSearchTermChange = (searchTerm: string) => {
-        setSearchTerm(searchTerm);
-    };
-
-    const handleSearchBtnClick = async () => {
-        setSavedQuery(searchTerm);
-        await getResults(searchTerm);
-    };
-
-    const simulateError = () => {
-        setErrorCounter((prev) => prev + 1);
-    };
-
     return (
-        <div className="app">
-            <SearchSection
-                searchTerm={searchTerm}
-                onSearchTermChange={handleSearchTermChange}
-                onSearch={handleSearchBtnClick}
-            />
-            <ResultsSection searchResults={searchResult} isReady={!isLoading} />
-            <ErrorImitationBtn onclick={simulateError} />
-        </div>
+        <React.StrictMode>
+            <RouterProvider router={router} />
+        </React.StrictMode>
     );
 }
