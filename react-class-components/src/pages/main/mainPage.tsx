7debf1424fe0c2945e1a1df1395e533cb4709c0b
Svetlana <svetailina1987@gmail.com>
2024-07-23 18:01:53 +0300

    fix: change useGetCharactersQuery arguments to avoid unnecessary API calls

--- react-class-components/src/pages/main/mainPage.tsx
+++ react-class-components/src/pages/main/mainPage.tsx
@@ -16,7 +16,7 @@ export default function MainPage() {
     const [errorCounter, setErrorCounter] = useState(0);
     const [, setSearchParams] = useSearchParams();
     const [currentPage, setCurrentPage] = useState(1);
-    const { data: searchResult } = useGetCharactersQuery({ page: currentPage, searchTerm });
+    const { data: searchResult } = useGetCharactersQuery({ page: currentPage, searchTerm: savedQuery });
     const totalPages = searchResult?.itemsCount ? Math.ceil(searchResult.itemsCount / Page.TOTAL) : 0;
     const location = useLocation();
 
