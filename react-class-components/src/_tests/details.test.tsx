f4d65f62c44c666e9b3ca5f7cfb41d456689fa27
Svetlana <svetailina1987@gmail.com>
2024-08-01 17:43:00 +0300

    test: update test for Details

--- react-class-components/src/_tests/details.test.tsx
+++ react-class-components/src/_tests/details.test.tsx
@@ -1,11 +1,13 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import DetailPage from '../pages/detailPage/detailPage';
-import { describe, it, expect, vi, beforeEach } from 'vitest';
-import { MemoryRouter } from 'react-router-dom';
+import { describe, it, expect, beforeEach, Mock, vi } from 'vitest';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from '../utils/apiSlice';
 import currentPageReducer from '../utils/currentPageSlice';
+import Details from '../components/details/details';
+import { mockSearchResults } from './mockData';
+import React from 'react';
+import { useRouter } from 'next/router';
 
 let store = configureStore({
     reducer: {
@@ -27,21 +29,11 @@ beforeEach(() => {
     });
 });
 
-vi.mock('react-router-dom', async (importOriginal) => {
-    const actual = await importOriginal<typeof import('react-router-dom')>();
-    return {
-        ...actual,
-        useParams: () => ({ name: 'Luke Skywalker' }),
-    };
-});
-
 describe('DetailPage Component', () => {
     it('should render detail page with correct character information', async () => {
         render(
             <Provider store={store}>
-                <MemoryRouter>
-                    <DetailPage />
-                </MemoryRouter>
+                <Details initialDetailData={mockSearchResults} />
             </Provider>
         );
 
@@ -55,31 +47,25 @@ describe('DetailPage Component', () => {
             expect(description).toBeInTheDocument();
         });
     });
-    it('loading indicator is displayed while fetching data', async () => {
-        const { container } = render(
-            <Provider store={store}>
-                <MemoryRouter>
-                    <DetailPage />
-                </MemoryRouter>
-            </Provider>
-        );
-        const loader = container.querySelector('.loader-wrapper');
-        expect(loader).toBeInTheDocument();
-        await waitFor(() => {
-            const loaderAfterLoad = container.querySelector('.loader-wrapper');
-            expect(loaderAfterLoad).not.toBeInTheDocument();
-        });
-    });
-    it('clicking the close button hides the component', () => {
+
+    it('clicking the close button hides the component', async () => {
+        const push = vi.fn();
+
+        (useRouter as Mock).mockImplementation(() => ({
+            query: { page: 1, details: 'Luke+Skywalker' },
+            push,
+        }));
         render(
             <Provider store={store}>
-                <MemoryRouter>
-                    <DetailPage />
-                </MemoryRouter>
+                <Details initialDetailData={mockSearchResults} />
             </Provider>
         );
+
         const closeBtn = screen.getByRole('button');
         fireEvent.click(closeBtn);
-        expect(screen.queryByText(/Luke Skywalker/i)).not.toBeInTheDocument();
+
+        expect(push).not.toHaveBeenCalledWith({
+            query: { details: 'Luke+Skywalker' },
+        });
     });
 });
