43190c1e2319272e3e87e64bd1d058370e82bea9
Svetlana <svetailina1987@gmail.com>
2024-08-02 13:04:56 +0300

    test: add test for gssp

--- react-class-components/vitestSetup.ts
+++ react-class-components/vitestSetup.ts
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import { setupServer } from 'msw/node';
 import { http, HttpResponse } from 'msw';
-import { emptyMockResults, mockResults, mockSearchResults } from './src/_tests/mockData';
+import { emptyMockResults, mockResults, mockResultsSearch, mockSearchResultsLuke } from './src/_tests/mockData';
 import { beforeAll, afterEach, afterAll, vi } from 'vitest';
 
 const handlers = [
@@ -13,7 +13,10 @@ const handlers = [
             return HttpResponse.json(emptyMockResults, { status: 200 });
         }
         if (search === 'Luke%20Skywalker') {
-            return HttpResponse.json(mockSearchResults, { status: 200 });
+            return HttpResponse.json(mockSearchResultsLuke, { status: 200 });
+        }
+        if (search === 'Darth Vader') {
+            return HttpResponse.json(mockResultsSearch, { status: 200 });
         }
 
         return HttpResponse.json(mockResults, { status: 200 });
