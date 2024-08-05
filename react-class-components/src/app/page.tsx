5f4f41e0f045ecf8449b6456f2f004114d7e3b9d
Svetlana <svetailina1987@gmail.com>
2024-08-05 17:55:57 +0300

    feat: add request errors handling

--- react-class-components/src/app/page.tsx
+++ react-class-components/src/app/page.tsx
@@ -1,6 +1,6 @@
 import MainPage from '../components/mainPage/mainPage';
 import { PageCount } from '../types/enums';
-import { SwapiPerson } from '../types/type';
+import { SwapiPeopleResponse, SwapiPerson } from '../types/type';
 import fetchData from '../utils/swapi';
 
 export interface PageParams {
@@ -11,13 +11,25 @@ export default async function Page({ searchParams }: PageParams) {
     const searchTerm = searchParams.searchTerm;
     const page = searchParams.page;
     const details = searchParams.details;
-    const resultData = await fetchData(searchTerm, page);
-    const totalPages = Math.ceil(resultData.count / PageCount.TOTAL);
     let detailsData: SwapiPerson[] = [];
+    let resultData: SwapiPeopleResponse = {
+        count: 0,
+        next: null,
+        previous: null,
+        results: [],
+    };
+    let totalPages = 0;
 
-    if (details) {
-        const data = await fetchData(details);
-        detailsData = data.results;
+    try {
+        resultData = await fetchData(searchTerm, page);
+        totalPages = Math.ceil(resultData.count / PageCount.TOTAL);
+
+        if (details) {
+            const data = await fetchData(details);
+            detailsData = data.results;
+        }
+    } catch (error) {
+        console.error(error);
     }
 
     return <MainPage initialData={resultData.results} initialDetailData={detailsData} totalPages={totalPages} />;
--- react-class-components/src/components/resultSection/resultSection.tsx
+++ react-class-components/src/components/resultSection/resultSection.tsx
@@ -11,12 +11,10 @@ export default function ResultsSection({ initialData }: ResultsSectionProps) {
     const { isDark } = useTheme();
 
     let content: React.ReactNode;
-    if (initialData) {
-        if (initialData.length > 0) {
-            content = initialData.map((result, index) => <Card key={index} character={result} />);
-        } else {
-            content = <p className={styles.noResult}>No results found</p>;
-        }
+    if (initialData && initialData.length > 0) {
+        content = initialData.map((result, index) => <Card key={index} character={result} />);
+    } else {
+        content = <p className={styles.noResult}>No results found</p>;
     }
 
     return <div className={`${styles.leftSection} ${isDark ? styles.leftSectionDark : ''}`}>{content}</div>;
