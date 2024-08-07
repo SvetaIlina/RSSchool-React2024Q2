3dbca8f2cb299fcc37f6e4dc4d18e7095f520bb8
Svetlana <svetailina1987@gmail.com>
2024-08-07 13:39:32 +0300

    feat: add the Remix framework

--- react-class-components/vite.config.ts
+++ react-class-components/vite.config.ts
@@ -2,11 +2,17 @@
 /// <reference types="vite/client" />
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { vitePlugin as remix } from '@remix-run/dev';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        remix({
+            ignoredRouteFiles: ['**/*.css'],
+        }),
+        tsconfigPaths(),
+    ],
     test: {
         globals: true,
         environment: 'jsdom',
